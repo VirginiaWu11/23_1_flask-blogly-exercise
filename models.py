b3259c9a2109154802652f1396fb0b501c63651c
Virginia Wu <virginiawu@virginias-air.myfiosgateway.com>
2021-05-03 22:15:24 -0400

    add tagging functionality

--- models.py
+++ models.py
@@ -34,7 +34,31 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     
     users = db.relationship('User', backref='posts')
+    posttag = db.relationship('PostTag', backref='posts')
+    tags = db.relationship('Tag', secondary='posts_tags', backref='posts')
 
     def __repr__(self):
         p=self
         return f"<Post id= {p.id}  title= {p.title}  content= {p.content} user_id= {p.user_id}"
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True) 
+    name = db.Column(db.String, nullable = False, unique=True)
+    
+    posttag = db.relationship('PostTag', backref='tags')
+
+    def __repr__(self):
+        t=self
+        return f"<Tag id= {t.id}  name= {t.name}>"
+
+
+class PostTag(db.Model):
+    """Mapping of a post to a tag"""
+    __tablename__ = 'posts_tags'
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key = True) 
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key = True)
+    
+    
